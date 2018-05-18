abc700a404bdd7680278c8ce5e0bc581e715ee6d
Jiasen Li <fkl7619480@gmail.com>
2018-05-19 01:26:55 +0800

    文本數據結構化
    cf:there has some unittest problem

--- DataProcessing/src/segmentWords.py
+++ DataProcessing/src/segmentWords.py
@@ -1,27 +1,57 @@
 import jieba
 import csv
+from gensim import corpora, models
+from gensim.matutils import corpus2dense
 
 def cutTextFile(path = ""):
     with open (path, "rt", encoding = 'utf-8') as file:
-        content = file.read()
+        content = file.read()#有暫存問題v.s readline
         words = jieba.lcut(content)
-    return " ".join(words)
+    return words
 
-def cutCsvFile(path = ""):
-    dataDict = openCsvFileAsDict(path)
+def cutCsvFile(dataDict):
     for row in dataDict:
-        row['text'] = " ".join(jieba.lcut(row['text']))
+        row['text'] = jieba.lcut(row['text'])
     return dataDict
 
-def openCsvFileAsDict(path = ""):
+def stopwordsFilter(sentence, stopwords):
+    result = []
+    for seg in sentence:
+        if seg not in stopwords: #過濾停用辭
+            result.append(seg)
+    return result
+
+def openCsvFileAsDict(path = "", cutText = False, stopwords = None):
     with open (path, "rt", encoding = 'utf-8') as file:
         reader = csv.DictReader(file, fieldnames = ['time', 'id', 'text', 'share', 'likecount', 'sharecount'])
-        content = [row for row in reader]#字典串列生成式
-
+        content = [row for row in reader] #字典串列生成式
     if (content[0]['likecount'] == '讚數'): #移除資料說明欄
         del content[0]
 
+    if(cutText):
+        content = cutCsvFile(content)
+        # if(stopwords != None):
+        #     for row in content:
+        #         row['text'] = stopwordsFilter(content, stopwords)
+
     return content
+
+def createDtm(texts):#建立文檔辭頻矩陣
+    dictionary = corpora.Dictionary(texts)
+    wordCount = [dictionary.doc2bow(text) for text in texts]#list of (wordID,count)
+    dtm = corpus2dense(wordCount, len(dictionary))#one_hot_represent matrix
+    return dtm.T
+
+def createTfidf(texts):#辭頻-逆向文檔辭頻
+    dictionary = corpora.Dictionary(texts)
+    wordCount = [dictionary.doc2bow(text) for text in tests]#list of (wordID,count)
+    tfidfModel = models.tfidfModel(wordCount)
+    tfidf = tfidfModel[wordCount]
+    tfidfMatrix = corpus2dense(tfidf, len(dictionary))#(data,featureCount)
+    return tfidfMatrix.T
+
+# def creatDTVectorSpace(texts):#distributed representation
+    # modelDR = gensim.models.Word2Vec(texts, size=100, window=5, min_count=5)
 
 
 if __name__ == "__main__":
--- DataProcessing/test/segment_test.py
+++ DataProcessing/test/segment_test.py
@@ -1,5 +1,6 @@
 import unittest
 from ..src import segmentWords
+import numpy as np
 
 class TestSegment(unittest.TestCase):
     TEXT_FILE_PATH = "DataProcessing/test_data/testData.txt"
@@ -7,7 +8,7 @@ class TestSegment(unittest.TestCase):
 
     def test_cutTextFile(self):
         segmented = segmentWords.cutTextFile(self.TEXT_FILE_PATH)
-        self.assertEqual("智者 守時 而 盡 其智 ， 不肖 者 守命 而 盡 其力 。 \n", segmented)
+        self.assertEqual("智者 守時 而 盡 其智 ， 不肖 者 守命 而 盡 其力 。 \n", " ".join(segmented))
 
     def test_openCsvFileAsDict(self):
         dataDict = segmentWords.openCsvFileAsDict(self.CSV_FILE_PATH)
@@ -18,5 +19,30 @@ class TestSegment(unittest.TestCase):
         self.assertEqual('124', dataDict[0]['sharecount'])
 
     def test_cutCsvFile(self):
-        dataDict = segmentWords.cutCsvFile(self.CSV_FILE_PATH)
-        self.assertEqual('適當 的 使用 動畫 可以 為 你 的 作品 加 分 ， 但動畫 不是 人人 都 會 做 ， 所以 這次 我們 為 大家 介紹 如何 透過   loading . io   圖示 庫 快速 製 作 屬 於 自己 的 動畫 圖示 。 \n \n 全文 :   http : / / blog . infographics . tw / 2018 / 02 / loading - io - animated - icons /', dataDict[0]['text'])
+        dataDict = segmentWords.openCsvFileAsDict(self.CSV_FILE_PATH, cutText = True)
+        self.assertEqual('適當 的 使用 動畫 可以 為 你 的 作品 加 分 ， 但動畫 不是 人人 都 會 做 ， 所以 這次 我們 為 大家 介紹 如何 透過   loading . io   圖示 庫 快速 製 作 屬 於 自己 的 動畫 圖示 。 \n \n 全文 :   http : / / blog . infographics . tw / 2018 / 02 / loading - io - animated - icons /', " ".join(dataDict[0]['text']))
+
+    def test_stopwordsFilter(self):
+        victimText = ["帥哥", "是", "你", "#", "媽的", "@", "智障"]
+        stopwords = ["#", "@", "是", "不是"]
+        result = " ".join(segmentWords.stopwordsFilter(victimText, stopwords))
+        self.assertEqual("帥哥 你 媽的 智障", result)
+
+    # def test_cutCsvFileWithStopwords(self):
+    #     stopwords = ["#", "@", "是", "不是"]
+    #     dataDict = segmentWords.openCsvFileAsDict(self.CSV_FILE_PATH, cutText = True, stopwords = stopwords)
+    #     self.assertEqual('適當 的 使用 動畫 可以 為 你 的 作品 加 分 ， 但動畫 不是 人人 都 會 做 ， 所以 這次 我們 為 大家 介紹 如何 透過   loading . io   圖示 庫 快速 製 作 屬 於 自己 的 動畫 圖示 。 \n \n 全文 :   http : / / blog . infographics . tw / 2018 / 02 / loading - io - animated - icons /', dataDict[0]['text'])
+
+    # def test_createDtm(self):
+    #     texts = [["智者", "守時", "而", "盡", "其智"],
+    #              ["不肖", "者", "守命", "而", "盡", "其力"]]
+    #     dtm = segmentWords.createDtm(texts)
+    #     expectResult = np.array([[1., 1., 1., 1., 1., 0., 0., 0., 0.],
+    #                              [0., 0., 0., 1., 1., 1., 1., 1., 1.]])
+    #     self.assertEqual(expectResult, dtm)
+
+
+    # def test_creareTfidf(self):
+    #     texts = [["智者", "守時", "而", "盡", "其智"],
+    #              ["不肖", "者", "守命", "而", "盡", "其力"]]
+    #     tfidf = segmentWords.createTfidf(texts)
