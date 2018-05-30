33b0cd649609d2561695f1881f1112131a36d0a0
Oscar Tzeng <104820003>
2018-05-30 16:19:04 +0800

    做一些整合，輸入介面優化

--- Web/crawl/facebook/mlab.py
+++ Web/crawl/facebook/mlab.py
@@ -1,21 +1,23 @@
 import pymongo
 from pymongo import MongoClient
-import csv
-import codecs
-#import os
-#from mongoengine import connect, Document, StringField,DateTimeField
+# import csv
+# import codecs
+# import os
+# from mongoengine import connect, Document, StringField,DateTimeField
 
 MONGO_HOST = "mongodb://104820022:104820022@ds034677.mlab.com:34677/python"
 
 
 DOCUMENT_NAME = "python"
-
+'''
 def mongoengine():
     # get the mlab URI from environment variables
     uri = os.getenv("mongodb://104820022:104820022@ds034677.mlab.com:34677/python")
 
     # connect to our database at MLAB
     connect(db='python',username='104820022',password='104820022',host=uri)
+'''
+
 
 def connect_mongo(collection_input):
     try:
@@ -26,6 +28,7 @@ def connect_mongo(collection_input):
         print('Got an error!')
         print(e)
 
+
 def getAllDoc(collection_input):
     try:
         collection = connect_mongo(collection_input)
@@ -33,21 +36,23 @@ def getAllDoc(collection_input):
     except Exception as e:
         print('Got an error!')
         print(e)
-
+'''
 def read_csv():
     reader = open(FILENAME, 'r', encoding='utf8')
     return csv.DictReader(reader, COLUMNS)
+'''
 
 
-def save_to_mongo(collection_input,input_data):
+def save_to_mongo(collection_input, input_data):
     collection = connect_mongo(collection_input)
-    #data = read_csv()
+    # data = read_csv()
 
     try:
         collection.drop()
         collection.insert_many(input_data)
-        #result = collection.insert_many(data)
-        #print('%d rows are saved to "%s" collection in "%s" document successfully!' % (len(result.inserted_ids), COLLECTION_NAME, DOCUMENT_NAME))
+        # result = collection.insert_many(data)
+        # print('%d rows are saved to "%s" collection in "%s" document successfully!' % (len(result.inserted_ids), COLLECTION_NAME, DOCUMENT_NAME))
     except Exception as e:
         print('Got an error!')
         print(e)
+
--- Web/crawl/facebook/views.py
+++ Web/crawl/facebook/views.py
@@ -1,27 +1,38 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from facebook.mlab import getAllDoc
 from facebook.crawlFB import crawl
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def text(request):
-	units = getAllDoc("post")
-	return render(request, "text.html", locals())	
+    units = getAllDoc("post")
+    return render(request, "text.html", locals())
+
+
+def help(request):
+    return render(request, 'help.html')
+
 
 def get(request):
-	if request.method == "POST":		#如果是以POST方式才處理
-		mess = request.POST['token']
-		mess2 = request.POST['page_id']
-		if mess == '':
-			mess = "請輸入token"
-		elif mess2 == '':
-			mess2 = "請輸入粉絲專頁ID"	
-		else:
-			crawl(mess, mess2)	
-			return redirect('/text/')	
-	else:
-		mess="token尚未送出!"	
-		mess2="粉絲專頁ID尚未送出!"
-	return render(request, "get.html", locals())
+    if request.method == "POST":		# 如果是以POST方式才處理
+        mess = request.POST['token']
+        mess2 = request.POST['page_id']
+        if mess == '':
+            mess = "請輸入token"
+        elif mess2 == '':
+            mess2 = "請輸入粉絲專頁ID"
+        else:
+            crawl(mess, mess2)
+            return redirect('/text/')
+    else:
+        mess = "token尚未送出!"
+        mess2 = "粉絲專頁ID尚未送出!"
+    return render(request, "get.html", locals())
+
+
+def comming_soon(request):
+    return render(request, "soon.html")
