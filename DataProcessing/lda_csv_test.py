101c16834f2a9f22a2e3f713a46d1b33ec3289b7
Oscar Tzeng <oscar217b@gmail.com>
2018-09-16 01:15:15 +0800

    優化Regex去除URL部分, 增加Emoji排除範圍
    TMD Emoji怎麼那麼多 訂Unicode的人是太閒嗎

---
+++ DataProcessing/lda_csv_test.py
@@ -0,0 +1,17 @@
+"""
+直接用Python Console在 DataProcessing\ 底下測試用
+用來測其他測資
+"""
+
+from src.corpora import Corpora
+from src.lda import Lda
+
+CSV_FILE_PATH = "test_data/cnanewstaiwan.csv"
+STOPWORDS_FILE_PATH = "src/stopwords.txt"
+
+corpora = Corpora(filePath=CSV_FILE_PATH, isDeleteUrl=False, stopwords=STOPWORDS_FILE_PATH)
+lda = Lda(corpora, numTopics=2, seed=10)
+print("lda.showTopicsStr()\n", lda.showTopicsStr(), "\n")
+print("lda.showTopicsList()[0][1]\n", lda.showTopicsList()[0][1], "\n")
+print("lda.classifyTopic()\n", lda.classifyTopic(), "\n")
+print("lda.findArticleMatched()\n", lda.findArticleMatched(), "\n")
