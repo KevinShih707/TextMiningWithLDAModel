d87394fab34bd75380bc3c376f474905c9035c68
Oscar Tzeng <oscar217b@gmail.com>
2018-10-10 01:35:05 +0800

    長條圖選擇時 加了box shadow

--- Web/crawl/static/js/barchart.js
+++ Web/crawl/static/js/barchart.js
@@ -26,34 +26,83 @@ nv.addGraph(function () {
         .call(chart);
 
     d3.select('.nv-x.nv-axis > g').selectAll('g')
-    .selectAll('text')
-    .style('text-align', "right")
-    .attr('transform', function(d,i,j) { return 'translate (0, 10)' });
-  
+        .selectAll('text')
+        .style('text-align', "right")
+        .attr('transform', function(d,i,j) { return 'translate (0, 10)' });
+
+    filter = d3.select('defs')
+        .append("filter")
+        .attr("id", "white-glow")
+        .attr("x", "-5000%")
+        .attr("y", "-5000%")
+        .attr("width", "10000%")
+        .attr("height", "10000%");
+        
+    filter.append('feFlood')
+        .attr('result', 'flood')
+        .attr('flood-color', '#ffffff')
+        .attr('flood-opacity', '1');
+
+    filter.append('feComposite')
+        .attr('in', 'flood')
+        .attr('result', 'mask')
+        .attr('in2', 'SourceGraphic')
+        .attr('operator', 'in');
+
+    filter.append('feMorphology')
+        .attr('in', 'mask')
+        .attr('result', 'dilated')
+        .attr('operator', 'dilate')
+        .attr('radius', '2');
+
+    filter.append('feGaussianBlur')
+        .attr('in', 'dilated')
+        .attr('result', 'blurred')
+        .attr('stdDeviation', '5');
+
+    feMerge = filter.append('feMerge')
+        .append('feMergeNode')
+        .attr('in', 'blurred');
+
+    feMerge.append('feMergeNode')
+        .attr('in', 'SourceGraphic');
+
     nv.utils.windowResize(chart.update);
 
     return chart;
 });
 
 document.addEventListener("DOMContentLoaded", function(e){
-    var bars = document.getElementsByClassName("nv-bar");
+    var bars = document.getElementsByClassName("discreteBar");
+    bars[0].style.filter = "url(#white-glow)";
     var i;
     for(i = 0; i < bars.length; i++){
-        console.log("var i", i);
         bars[i].setAttribute("onclick", "clickBar(" + i + ","+ bars.length + ")" );
     }
 })
 
 function clickBar(i, length){
     var id = "topic-" + i.toString();
-    console.log(i, length);
     document.getElementById(id).classList.remove("uk-hidden");
+    var clickedElement = document.getElementsByClassName("discreteBar");    
+    try{glow(clickedElement[i]);}
+    catch(e){console.log(e);}
     var j;
     for(j = 0; j < length; j++){
         if(j != i){
-            console.log(j)
+            unglow(clickedElement[j]);
             document.getElementById("topic-" + j.toString()).classList.add("uk-hidden");
         }
     }
+}
+
+function glow(clickedElement){
+    var style = clickedElement.style;
+    style.filter = "url(#white-glow)";
+}
+
+function unglow(clickedElement){
+    var style = clickedElement.style;
+    style.filter = "none";
 }
 
